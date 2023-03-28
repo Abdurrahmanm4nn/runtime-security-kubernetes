82f8aec4ba2bf806bc569c11b999130e02af219b
Aghniya Abdurrahman Mannan <abdurrahman1270@gmail.com>
2023-03-29 05:02:20 +0700

    fix SA creation script and generate kubeconfig

--- defense-system/deploy-function.sh
+++ defense-system/deploy-function.sh
@@ -3,8 +3,10 @@ SA_ACCOUNT=falco-falcosk-sa
 GOOGLE_PROJECT_ID=$(gcloud config get-value project)
 FUNCTION_NAME=KillPwnedPod
 
+# deploy function
 gcloud functions deploy $FUNCTION_NAME \
 --runtime go113 --trigger-http \
 --service-account $SA_ACCOUNT@$GOOGLE_PROJECT_ID.iam.gserviceaccount.com
 
+# check if function is created
 gcloud functions describe --format=json $FUNCTION_NAME | jq -r '.name'
--- defense-system/install-falco-falcosk.sh
+++ defense-system/install-falco-falcosk.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
+
 export FALCO_NAMESPACE=falco
 FUNCTION_NAME=KillPwnedPod
 
+# Get cloud function name
+CLOUD_FUNCTION_NAME=$(gcloud functions describe --format=json $FUNCTION_NAME | jq -r '.name')
+
+# Create new namespace for falco
 kubectl create namespace $FALCO_NAMESPACE
 
+# install falco using helm
 helm upgrade --install falco falcosecurity/falco \
 --namespace $FALCO_NAMESPACE \
 --set ebpf.enabled=true \
 --set falcosidekick.enabled=true \
 --set falcosidekick.config.gcp.cloudfunctions.name=${CLOUD_FUNCTION_NAME} \
 --set falcosidekick.webui.enabled=true
+
+# check if falco running
+kubectl logs -n falco $(kubectl get pod -n falco -l app=falco -o=name) -f
