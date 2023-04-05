1cbc6c78e15f848f83b48045d9158bda8b3732f5
Abdurrahmanm4nn <abdurrahman1270@gmail.com>
2023-04-05 14:20:45 +0700

    revision of falco deployment script

--- defense-system/install-falco-falcosk.sh
+++ defense-system/install-falco-falcosk.sh
@@ -3,6 +3,7 @@
 export FALCO_NAMESPACE=falco
 SA_ACCOUNT=falco-falcosk-sa
 FUNCTION_NAME=KillIlegalPod
+GOOGLE_PROJECT_ID=$(gcloud config get-value project)
 
 # Get cloud function name
 CLOUD_FUNCTION_NAME=$(gcloud functions describe --format=json $FUNCTION_NAME | jq -r '.name')
@@ -20,12 +21,12 @@ helm install falco falcosecurity/falco \
 --set ebpf.enabled=true \
 --set driver.kind=ebpf \
 --set falcosidekick.enabled=true \
---set falcosidekick.config.gcp.cloudfunctions.name=${CLOUD_FUNCTION_NAME} \
+--set falcosidekick.config.gcp.cloudfunctions.name="${CLOUD_FUNCTION_NAME}" \
 --set falcosidekick.webui.enabled=true
 
 # annotate falco-falcosidekick resource
 kubectl annotate serviceaccount --namespace $FALCO_NAMESPACE falco-falcosidekick \
-iam.gke.io/gcp-service-account=${SA_ACCOUNT}@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com
+iam.gke.io/gcp-service-account="${SA_ACCOUNT}@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com"
 
 # check if falco running
 kubectl get pod -n falco
